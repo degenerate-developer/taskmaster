0449637527ae3a2eaef62573f25db2adf6fd2cd7
jesse <the.jesse.dev@gmail.com>
2025-07-19 14:32:02 -0700

    cleanup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,52 +1,4 @@
-// import { Manrope } from "next/font/google";
-// import { Providers } from "@/components/providers";
-// import backgroundTexture from "@/public/background.png";
-// import { Toaster } from "@taskmaster/ui/components/sonner";
-// import localFont from "next/font/local";
-// import "@taskmaster/ui/globals.css";
-// import { Analytics } from "@vercel/analytics/next";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
-// import { metadata } from "./metadata";
-// // import { ReactScan } from "@/components/react-scan";
-
-// export { metadata };
-
-// const fontSans = Manrope({
-//   subsets: ["latin"],
-//   variable: "--font-sans",
-// });
-
-// const fontHeadline = localFont({
-//   src: "../public/fonts/tiempos-headline-light.woff2",
-//   style: "normal",
-//   variable: "--font-mono",
-//   display: "swap",
-//   fallback: ["Georgia", "Times New Roman", "serif"],
-// });
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body
-//         className={`${fontSans.variable} ${fontHeadline.variable} font-sans antialiased`}
-//         style={{
-//           backgroundImage: `url(${backgroundTexture.src})`,
-//           backgroundRepeat: "repeat",
-//         }}
-//       >
-//         {/* <ReactScan /> */}
-//         <Providers>{children}</Providers>
-//         <Toaster />
-//         <Analytics />
-//         <SpeedInsights />
-//       </body>
-//     </html>
-//   );
-// }
+import { Providers } from "@/components/providers";
 
 export default function RootLayout({
   children,
@@ -55,7 +7,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- apps/web/components/topnav.tsx
+++ apps/web/components/topnav.tsx
@@ -1,20 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
-import { ProfileContactsIcon, ProfileContacts } from "./profile-contacts";
 
 export const TopNav = () => {
   return (
     <nav className="mb-3 flex w-full items-center justify-between border-b py-4 px-8 text-xl font-medium">
-      <h1 className="font-mono">the residency</h1>
+      <h1 className="font-mono">taskmaster</h1>
       <div className="flex items-center">
-        <UserButton>
-          <UserButton.UserProfilePage
-            label="Public"
-            url="contact-info"
-            labelIcon={<ProfileContactsIcon />}
-          >
-            <ProfileContacts />
-          </UserButton.UserProfilePage>
-        </UserButton>
+        <UserButton />
       </div>
     </nav>
   );
